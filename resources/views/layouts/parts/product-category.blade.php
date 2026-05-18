71ac61972c5887349fe2f1895574ee64d473eb7d
nabilahunafa <nabilahunnafa02@gmail.com>
2026-05-18 20:39:47 +0700

    Update Dashbooard

--- resources/views/layouts/parts/product-category.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Admin Panel - Product Category')
-
-@section('content')
-    @include('contents.product-category-content')
-@stop
--- resources/views/layouts/parts/product.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Admin Panel - Product')
-
-@section('content')
-    @include('contents.product-content')
-@stop
